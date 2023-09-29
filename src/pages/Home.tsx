5e637f8454b5e527d4a98ba28b621856a57dfa8a
luckylucyj <jeyun96@gmail.com>
2023-09-29 17:07:42 +0900

    feat(JiYun): 메인 페이지 라우팅 및 공통 스타일 파일 제작

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import "../styles/style.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      {/* Header 메뉴 & 로고 */}
+      <Header />
+      <main className="wrapper">
+        {/* 1. 메인 섹션 */}
+        {/* 2. 내 주변 위치 */}
+        <h1>main page</h1>
+        {/* 3. 띠 배너 */}
+        {/* 4. 주제별 트레킹 */}
+
+      </main>
+      {/* Footer */}
+      <Footer/>
+    </>
+  );
+}
+
+export default Home;
