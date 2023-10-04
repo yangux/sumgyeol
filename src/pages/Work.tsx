6b65c7ed5afee346375fedfae9838f8943b57685
yangux <yeonie05@gmail.com>
2023-10-04 11:11:07 +0900

    feat(SuYeon): 작품 페이지 상품 카드 컴포넌트 생성

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,7 +1,19 @@
-export default function Work(){
-  return(
+import WorkItem from "../components/work/WorkItem";
+import { itemArr } from "../utils/workItems";
+
+export default function Work() {
+  return (
     <>
-    work
+      {itemArr.map((data, i) => (
+        <WorkItem
+          name={data.name}
+          originalPrice={data.originalPrice}
+          discountRate={data.discountRate}
+          reviewCount={data.reviewCount}
+          likeCount={data.likeCount}
+          key={i}
+        />
+      ))}
     </>
-  )
+  );
 }
