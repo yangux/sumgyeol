2f9620646b403eb47216f4b171eff7f40939e345
yangux <yeonie05@gmail.com>
2023-10-06 16:56:04 +0900

    design(SuYeon): 작품 페이지 카테고리 메뉴 추가

--- src/components/work/WorkItemGrid.tsx
+++ src/components/work/WorkItemGrid.tsx
@@ -2,31 +2,69 @@ import styled from "styled-components";
 import { itemArr } from "../../utils/workItemsdata";
 import WorkItem from "./WorkItem";
 
-const GridContainer = styled.div`
-  max-width: 1200px;
+const Section = styled.div`
+  max-width: 1120px;
   margin: 50px auto;
+`;
+const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   gap: 10px;
-  padding: 50px;
 `;
+const CategoryMenu = styled.ul`
+  display: flex;
+  margin-bottom: 20px;
+
+  li {
+    margin-right: 1.2rem;
+    color: var(--text-gray-50);
+    cursor: pointer;
+    &:not(li:last-child)::after {
+      content: "";
+      display: inline-block;
+      width: 1px;
+      height: 80%;
+      background-color: var(--text-gray-40);
+      margin-left: 1.2rem;
+    }
+  }
+`;
+
+const categories: string[] = [
+  "전체",
+  "옷",
+  "음식",
+  "악세사리",
+  "가구",
+  "식기",
+  "필기도구",
+  "악기",
+  "기타",
+];
 
 export default function WorkItemGrid() {
   return (
-    <GridContainer>
-      {itemArr.map((data, i) => (
-        <WorkItem
-          name={data.name}
-          brand={data.brand}
-          originalPrice={data.originalPrice}
-          discountRate={data.discountRate}
-          reviewCount={data.reviewCount}
-          likeCount={data.likeCount}
-          category={data.category}
-          image={data.image}
-          key={i}
-        />
-      ))}
-    </GridContainer>
+    <Section>
+      <CategoryMenu>
+        {categories.map((category, i) => (
+          <li>{category}</li>
+        ))}
+      </CategoryMenu>
+      <GridContainer>
+        {itemArr.map((data, i) => (
+          <WorkItem
+            name={data.name}
+            brand={data.brand}
+            originalPrice={data.originalPrice}
+            discountRate={data.discountRate}
+            reviewCount={data.reviewCount}
+            likeCount={data.likeCount}
+            category={data.category}
+            image={data.image}
+            key={i}
+          />
+        ))}
+      </GridContainer>
+    </Section>
   );
 }
